39f46a7d5d298b203bd56f38a961b238a089c1f1
Lucija Josipović Deranja <59958682+ljosipovicderanja@users.noreply.github.com>
2022-04-01 10:20:16 +0200

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <ul>
+      <li v-for="item in knjiga" v-bind:key="item.sha">
+        Knjiga <router-link :to="'/knjiga/' + item.sha">{{item.url + " , " + item.name + ", " + item.authors }}</router-link>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+
+  data() {
+    return {
+        knjiga: [],
+    }
+  },
+ async mounted() {
+    
+    
+    let rezultat = await fetch("https://www.anapioficeandfire.com/api/books");
+     
+     let podaci = await rezultat.json()
+    
+    for (let item of podaci) {
+     console.log(item.sha);
+    }
+  this.knjiga = podaci;
+  },
+  
+}
+</script>
